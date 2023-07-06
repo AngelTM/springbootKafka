ffe45cce141d041ac13eaeb9f6a455d606b116ed
AngelTM <angel1997amg@gmail.com>
2023-07-06 16:26:31 -0700

    first commit

---
+++ springbootprovider/src/test/java/com/example/provider/springbootprovider/SpringbootproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provider.springbootprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
